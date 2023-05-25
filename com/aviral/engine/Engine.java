19c19c0781ef934982cc729a9dc339f2567662ea
Developer Avi <developer.aviral@gmail.com>
2023-05-25 12:28:00 +0530

    LinkedList insert and display operations

--- com/aviral/engine/Engine.java
+++ com/aviral/engine/Engine.java
@@ -9,5 +9,17 @@ public class Engine {
         l.insert(50);
         l.insert(20);
         l.show();
+
+        System.out.println("Insert at first");
+        l.insertAtFirst(123);
+        l.show();
+
+        System.out.println("Insert at 2nd index");
+        l.insertAt(13,505);
+        l.show();
+
+        System.out.println("Delete at 3rd index");
+        l.deleteAt(10);
+        l.show();
     }
 }
--- com/aviral/list/LinkedList.java
+++ com/aviral/list/LinkedList.java
@@ -1,5 +1,6 @@
 package com.aviral.list;
 
+
 public class LinkedList {
     Node head;
     public void insert(int data){
@@ -16,6 +17,53 @@ public class LinkedList {
                 n = n.getNext();
             }
             n.setNext(node);
+        }
+    }
+
+    public void insertAtFirst(int data){
+        Node node = new Node();
+        node.setData(data);
+        node.setNext(head);
+        head = node;
+    }
+
+    public void insertAt(int index, int data){
+        Node node = new Node();
+        node.setData(data);
+        node.setNext(null);
+
+        if(index == 0){
+            insertAtFirst(data);
+            return;
+        }
+        Node n = head;
+        for(int i =0 ; i < index -1 ; i++){
+            if(n.getNext() == null)
+                break;
+            n = n.getNext();
+        }
+
+        Node temp = n.getNext();
+        n.setNext(node);
+        node.setNext(temp);
+    }
+
+    public void deleteAt(int index){
+        Node n = head;
+        for (int i = 0; i< index -1 ; i++){
+            if(n.getNext() == null) {
+                System.out.println("Give index in range");
+                break;
+            }
+            n = n.getNext();
+        }
+
+        Node temp = n.getNext();
+        if(temp != null) {
+            n.setNext(temp.getNext());
+        }
+        else{
+            n.setNext(null);
         }
     }
 
